fc07d3cb5e3a737bdcc287844821513e8b0fc1ce
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-09 01:04:11 +0300

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import {ThemeProvider} from "@/components/theme-provider"
 import NavBar from '../components/nav-bar'
+import Footer from '../components/footer'
 
 const inter = Inter({subsets: ['latin']})
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           >
             <NavBar />
             {children}
+            <Footer/>
           </ThemeProvider>
         </body>
       </html>
