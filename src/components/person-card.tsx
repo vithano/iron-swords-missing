956da85f4fdf9ded3f1ae873e13240036a36ff9d
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-08 22:15:06 +0300

    fix: make result cards clickable

--- src/components/person-card.tsx
+++ src/components/person-card.tsx
@@ -10,6 +10,7 @@ import {
 import Image from "next/image";
 import PersonData from "../app/utils/types";
 import {sanitizeImageUrl} from "@/lib/utils";
+import Link from "next/link";
 
 export function PersonCard(data: PersonData) {
     const {firstName, lastName, contactName, identifyingDetails, contactPhone, image, missingPhone, lastSeen, notes, status, id} = data
@@ -17,31 +18,33 @@ export function PersonCard(data: PersonData) {
     const email = 'ironswordsoperation@gmail.com';
     const emailToAdmin = `mailto:${email}?subject=${subject}`;
     return (
-        <Card className="w-[350px] text-right" >
-            <CardHeader>
-                <CardTitle>{firstName} {lastName} </CardTitle>
-                <CardDescription>נראה לאחרונה {lastSeen}</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <div className="grid w-full items-center gap-4">
-                    <div className="flex flex-col space-y-1.5">
-                        <Image className="aspect-[14/13] w-full rounded-2xl object-fill cursor-pointer"
-                            width={350}
-                            height={350}
-                            onClick={()=>window.open(image, '_blank')}
-                            src={sanitizeImageUrl(image)} alt="person image" />
+        <Link href={`/profile/${id}`}>
+            <Card className="w-[350px] text-right" >
+                <CardHeader>
+                    <CardTitle>{firstName} {lastName} </CardTitle>
+                    <CardDescription>נראה לאחרונה {lastSeen}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Image className="aspect-[14/13] w-full rounded-2xl object-fill cursor-pointer"
+                                width={350}
+                                height={350}
+                                onClick={() => window.open(image, '_blank')}
+                                src={sanitizeImageUrl(image)} alt="person image" />
+                        </div>
                     </div>
-                </div>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-                {/* <Button 
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    {/* <Button 
                 onClick={() => window.open(`https://wa.me/+972${contactPhone}?text=היי%20אני%20רוצה%20לדווח%20על%20אדם%20שנראה%20לאחרונה%20אשמח%20אם%20תצרי%20איתי%20קשר%20לגבי%20זה`, '_blank')}
                 >צור קשר</Button> */}
-                <Button 
-                
-                onClick={()=> window.open(emailToAdmin)}
-                variant="outline">עדכן אותנו</Button>
-            </CardFooter>
-        </Card>
+                    <Button
+
+                        onClick={() => window.open(emailToAdmin)}
+                        variant="outline">עדכן אותנו</Button>
+                </CardFooter>
+            </Card>
+        </Link>
     )
 }
