183b835a5f262bc1cca9e54f828c1def9f468478
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-08 23:47:21 +0300

    feat: add faq

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button"
 
 export default function About() {
   const email = 'ironswordsoperation@gmail.com';
+  
   const emailToAdmin = `mailto:${email}`;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24" style={{direction:'rtl'}}>
@@ -12,7 +13,7 @@ export default function About() {
     <p className={"pb-2"}>האתר נועד לעזור ולאתר את הנעדרים של מלחמת חרבות ברזל
     האתר נבנה ללא מטרות רווח והוא פתוח לשימוש חופשי
     </p>
-    <p className="pb-2">כל אחד יכול להכניס את הפרטים של הנעדר שלו והאתר יציג את הנעדרים שמתאימים לפרטים שהוכנסו</p>
+    <p className="pb-2">האתר נבנה בפרויקט קוד פתוח וכל המידע בו מוזן על ידי מתנדבים</p>
     <Button onClick={()=> window.open(emailToAdmin)}
         className={"w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4"}>
         צור קשר
