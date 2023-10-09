f401b206cf057c4772b6637efe21c65ad13eced8
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-09 08:45:20 +0300

    fix: another og test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,18 +7,40 @@ import Footer from '../components/footer'
 
 const inter = Inter({subsets: ['latin']})
 
-export const metadata: Metadata = {
-  title: ' חרבות ברזל - איתור ועדכון נעדרים',
-  description: ' חרבות ברזל - איתור ועדכון נעדרים',
-  openGraph: {
-    images: [
-      {
-        url: 'https://iron-swords-missing.vercel.app/logo.jpg',
-        width: 1024,
-        height: 1024,
-        alt: 'Iron Swords',
-      },
-    ],
+
+export async function generateMetadata(): Promise<Metadata> {
+  const url = "https://iron-swords-missing.vercel.app/"
+  const title = "חרבות ברזל - איתור ועדכון נעדרים";
+  const desc = "חרבות ברזל - איתור ועדכון נעדרים";
+
+  const ogUrl = new URL(`${url}/api/og`)
+  ogUrl.searchParams.set("title", title)
+  ogUrl.searchParams.set("type", title)
+  ogUrl.searchParams.set("mode", "dark")
+
+  return {
+    title,
+    description: desc,
+    openGraph: {
+      title: title,
+      description: desc,
+      type: "article",
+      url,
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: title,
+      description: desc,
+      images: [ogUrl.toString()],
+    },
   }
 }
 
@@ -39,7 +61,7 @@ export default function RootLayout({
           >
             <NavBar />
             {children}
-            <Footer/>
+            <Footer />
           </ThemeProvider>
         </body>
       </html>
