1b548a47ab51e1e7f9ee275c5168dd49b76e1bff
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-09 11:54:55 +0300

    fix: make image smaller

--- src/components/person-card.tsx
+++ src/components/person-card.tsx
@@ -32,13 +32,12 @@ export function PersonCard(data: PersonData) {
                 <CardContent>
                     <div className="grid w-full items-center gap-4">
                         <div className="flex flex-col space-y-1.5">
-                            <Image className="aspect-[14/13] dark:drop-shadow-[0_0_0.25rem_#ffffff70] w-full rounded-2xl object-cover cursor-pointer"
-                                width={300}
-                                height={300}
-                                unoptimized={true}
-
+                            <Image className="aspect-[14/13] max-w-100 dark:drop-shadow-[0_0_0.25rem_#ffffff70] w-full rounded-2xl object-cover cursor-pointer"
+                                width={200}
+                                height={200}
                                 style={{objectPosition: '0 22%'}}
-                                src={sanitizeImageUrl(image)} alt="person image" />
+                                src={
+                                    sanitizeImageUrl(image)} alt={firstName + " " + lastName} />
                         </div>
                     </div>
                 </CardContent>
