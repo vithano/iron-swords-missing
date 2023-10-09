edc266ac3500f6f5c2f987c2f01ea1550501e169
idandagan1 <idand@wix.com>
2023-10-09 22:54:57 +0300

    fix: add validator for sql injection

--- src/services/fetch-db-data.ts
+++ src/services/fetch-db-data.ts
@@ -1,5 +1,6 @@
-import {PersonData} from '../app/utils/types';
+import { PersonData } from '../app/utils/types';
 import supabase from './supabase';
+import validator from 'validator';
 
 type Props = {
   name?: string;
@@ -9,34 +10,31 @@ type Props = {
 const MIN_QUERY_LENGTH = 3;
 
 export async function fetchDbData(props?: Props): Promise<PersonData[]> {
-  const {name, id} = props ?? {name: '', id: null}
-  const isFullname = name?.includes(' ');
-  const firstName = (isFullname ? name?.split(' ')[0] : name)?.trim();
-  const lastName = (isFullname ? name?.split(' ')[1] : name)?.trim();
+  const { name, id } = props ?? { name: '', id: null };
+  const isFullName = name?.includes(' ');
+  const sanitizedName = validator.escape(name || '');
+  const firstName = (isFullName ? sanitizedName.split(' ')[0] : sanitizedName).trim();
+  const lastName = (isFullName ? sanitizedName.split(' ')[1] : sanitizedName).trim();
 
-
-  if (name && name?.length < MIN_QUERY_LENGTH && !id)
+  if (sanitizedName && sanitizedName.length < MIN_QUERY_LENGTH && !id)
     return [];
 
   if (!firstName && !lastName && !id)
     return [];
 
-
-
   const nameQuery = `first_name.ilike.%${firstName}%,last_name.ilike.%${lastName}%`;
-  const idQuery = id ? `id.eq.${id}` : '';
+  const idQuery = id ? `id.eq.${validator.escape(id)}` : '';
 
-  let query = isFullname ? `and(${nameQuery})` : `or(${nameQuery})`;
+  let query = isFullName ? `and(${nameQuery})` : `or(${nameQuery})`;
   if (id) query = `or(${idQuery})`;
 
-
-  const {data = []} = await supabase
-    .from('people')
-    .select('*')
-    .or(query);
+  const { data = [] } = await supabase
+      .from('people')
+      .select('*')
+      .or(query);
 
   // @ts-ignore
-  return data?.map(({id, contact_name, contact_phone, details, first_name, image, last_name, last_seen, notes, status}) => ({
+  return data?.map(({ id, contact_name, contact_phone, details, first_name, image, last_name, last_seen, notes, status }) => ({
     id,
     contactName: contact_name,
     contactPhone: contact_phone,
@@ -47,10 +45,5 @@ export async function fetchDbData(props?: Props): Promise<PersonData[]> {
     lastSeen: last_seen,
     notes,
     status,
-  })) ?? []
-
-
-
-
+  })) ?? [];
 }
-
