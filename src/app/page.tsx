45f60038d4a8a71d78a7deb2d0988eb696847e62
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-09 09:51:18 +0300

    fix: typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <main className="flex flex-col min-h-screen p-4 sm:p-16">
-      <h1 className='text-center text-xl pb-8'> עזרה בעיתור נעדרים וחטופים</h1>
+      <h1 className='text-center text-xl pb-8'> עזרה באיתור נעדרים וחטופים</h1>
       {data.length == 0 && <Image
         className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert self-center"
         src="/logo.jpg"
