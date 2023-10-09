996590901e7d47e8e715ef6c817a2cebc5277a80
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-09 11:15:34 +0300

    fix: build is failing

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,8 +3,8 @@ import {createClient} from '@supabase/supabase-js'
 import {Database} from './database.types'
 
 
-const supabaseUrl = process.env.SUPABASE_URL ?? ""
-const supabaseKey = process.env.SUPABASE_KEY ?? ""
+const supabaseUrl = process.env.SUPABASE_URL || ""
+const supabaseKey = process.env.SUPABASE_KEY || ""
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 
