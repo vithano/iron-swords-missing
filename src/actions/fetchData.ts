b3cf2c58561b1ba11f3c19247be6cd7c0bf458a5
vithano <vithano@gmail.com>
2023-10-10 17:24:54 +0300

    try to clear cookies

--- src/actions/fetchData.ts
+++ src/actions/fetchData.ts
@@ -1,7 +1,8 @@
 'use server'
 
 import { fetchDbData } from '@/services/fetch-db-data';
-
+import { cookies } from 'next/headers'
 export async function fetchData({name}: {name: string}) {
+  const _cookies = cookies()
   return await fetchDbData({name})
 }
