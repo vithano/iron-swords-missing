ecca286140509373cad9c97ca114714fdbd17a86
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-11 01:34:00 +0300

    feat: integrate safedonate

---
+++ src/app/safedonate/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <div className="bg-red-800 text-white text-center text-2xl font-bold p-4">אינו באחריות האתר safedonate שימו לב ❤️ פרויקט </div>
+
+            <iframe className="h-screen w-screen bg-red-300" src="https://naboo.ai/safedonate" style={{border: 'none'}}></iframe>;
+        </div>);
+}
