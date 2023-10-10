e99f9b09b93c568e9b9eedb0a90ed7581e1ad771
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-10 21:45:44 +0300

    chore: lint

--- src/components/ui/sendEmailToAdminButton.tsx
+++ src/components/ui/sendEmailToAdminButton.tsx
@@ -1,17 +1,23 @@
 'use client';
 
-import { mailAdmin } from "@/lib/utils";
-import { Button } from "./button";
-import { Dialog } from "./dialog";
+import {mailAdmin} from "@/lib/utils";
+import {Button} from "./button";
+import {Dialog} from "./dialog";
 import CopyButton from "./copyButton";
 
-const SendEmailToAdminButton = ({children,text = 'צור קשר',subject}:{children?:React.ReactNode,text?:string,subject?:string}) =>{
-const adminMail = 'ironswordsoperation@gmail.com';
-const content = `אם לא ניתן לשלוח מייל דרך הכפתור אנא שלחו לנו ב ${adminMail}`
-const title = 'שלחו לנו מייל';
-const description = <span>נשמח לעזור לכם לעדכן את הנעדרים שלכם.<br/> {content}</span>;
-const primaryButton = <Button onClick={()=>mailAdmin(subject)}>שלח מייל</Button>
-const secondaryButton = <CopyButton text={adminMail}/>
-    return (<Dialog secondaryButton={secondaryButton} description={description} primaryButton={primaryButton} title={title}>{children? children : <Button variant="outline">{text}</Button>}</Dialog>)
+const SendEmailToAdminButton = ({
+    children,
+    text = 'צור קשר',
+    subject
+}: {children?: React.ReactNode, text?: string, subject?: string}) => {
+    const adminMail = 'ironswordsoperation@gmail.com';
+    const content = `אם לא ניתן לשלוח מייל דרך הכפתור אנא שלחו לנו ב ${adminMail}`
+    const title = 'שלחו לנו מייל';
+    const description = <span>נשמח לעזור לכם לעדכן את הנעדרים שלכם.<br /> {content}</span>;
+    const primaryButton = <Button onClick={() => mailAdmin(subject)}>שלח מייל</Button>
+    const secondaryButton = <CopyButton text={adminMail} />
+    return (
+        <Dialog secondaryButton={secondaryButton} description={description} primaryButton={primaryButton} title={title}>{children ? children : <Button variant="outline">{text}</Button>}
+        </Dialog>)
 }
 export default SendEmailToAdminButton;
