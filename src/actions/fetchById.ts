9e5be1e6be7d46e136693d4d2a3cb6cf7d1f85d9
vithano <vithano@gmail.com>
2023-10-10 17:20:37 +0300

    remove revalidate

--- src/actions/fetchById.ts
+++ src/actions/fetchById.ts
@@ -1,7 +1,6 @@
 'use server'
 
 import {fetchDbData} from '@/services/fetch-db-data';
-export const revalidate = 0;
 export async function fetchById({id}: {id: string}) {
   const data = await fetchDbData({id});
   return data[0];
--- src/actions/fetchData.ts
+++ src/actions/fetchData.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import { fetchDbData } from '@/services/fetch-db-data';
-export const revalidate = 0;
+
 export async function fetchData({name}: {name: string}) {
   return await fetchDbData({name})
 }
