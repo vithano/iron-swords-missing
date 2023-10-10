2ce391f58286294bdc429eb9dc9e89e3f6e0b678
vithano <vithano@gmail.com>
2023-10-10 20:46:43 +0300

    add more logs

--- src/app/api/notify/route.tsx
+++ src/app/api/notify/route.tsx
@@ -7,4 +7,16 @@ export const runtime = 'edge';
 export async function POST(request: Request) {
     //const {searchParams} = new URL(request.url);
     console.log(request)
+    const data = await request.json();
+    console.log(data);
+
+    // Now you can process the data...
+    // For example, if Supabase sends the updated row data, you could extract it like so:
+    const updatedRow = data.new;
+    console.log(updatedRow);
+    return new Response('Webhook handled', {
+        headers: {
+            'content-type': 'text/plain',
+        },
+    });
 }
