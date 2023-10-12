3a4bd4994ef55659272a0859a1ca22f83383862e
vithano <vithano@gmail.com>
2023-10-12 15:44:32 +0300

    sanitize id

--- src/services/fetch-db-data.ts
+++ src/services/fetch-db-data.ts
@@ -9,22 +9,26 @@ type Props = {
 const MIN_QUERY_LENGTH = 3;
 export async function fetchDbData(props?: Props): Promise<PersonData[]> {
   const { name, id } = props ?? { name: "", id: null };
+  
   const isFullName = name?.includes(" ");
   const isHebrewEnglishOrWhiteSpace = /^[\u0020\u0041-\u005A\u0061-\u007A\u0590-\u05FF]+$/.test(name || "");
   const sanitizedName = validator.escape(name || "");
-  if(sanitizedName && !isHebrewEnglishOrWhiteSpace) return [];
+  const isIdValid = validator.isAlphanumeric(id || "");
+  if(sanitizedName && (!isHebrewEnglishOrWhiteSpace || sanitizedName.length < MIN_QUERY_LENGTH)){
+    return [];
+  }
+  if(id && !isIdValid){
+    return [];
+  }
   const firstName = (
     isFullName ? sanitizedName.split(" ")[0] : sanitizedName
   ).trim();
   const lastName = (
     isFullName ? sanitizedName.split(" ")[1] : sanitizedName
   ).trim();
-
-  if (sanitizedName && sanitizedName.length < MIN_QUERY_LENGTH && !id)
+  if(!firstName && !lastName && !id){
     return [];
-
-  if (!firstName && !lastName && !id) return [];
-
+  }
   const nameQuery = `first_name.ilike.%${firstName}%,last_name.ilike.%${lastName}%`;
   const idQuery = id ? `id.eq.${validator.escape(id)}` : "";
 
