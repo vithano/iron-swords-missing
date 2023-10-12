203c3e274e7817baa7229c4d1c08cab250c7dc78
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-12 14:24:48 +0300

    refactor: migrate `actions` to kebab-case

--- src/actions/fetchById.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-import { cookies } from 'next/headers'
-
-import {fetchDbData} from '@/services/fetch-db-data';
-export async function fetchById({id}: {id: string}) {
-  const _cookies = cookies()
-  const data = await fetchDbData({id});
-  return data[0];
-}
---
+++ src/actions/remove-notifications.ts
@@ -0,0 +1,28 @@
+"use client";
+
+export async function removeNotification({
+  hash,
+  blacklist = false,
+}: {
+  hash: string;
+  blacklist?: boolean;
+}) {
+  if (blacklist) {
+    const data = await fetch(`/api/notify/blacklist`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ hash }),
+    });
+    return data;
+  }
+  const data = await fetch(`/api/notify`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ hash }),
+  });
+  return data;
+}
