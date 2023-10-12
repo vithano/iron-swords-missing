e0d71d5386848185a074bdbc588b2795925bbb19
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-10-12 12:05:51 +0300

    fix: duplicate in next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'drive.google.com',
-            },
-          ],      
-    },
-    experimental: {
-        serverActions: true,
-    },
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'drive.google.com',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        port: "",
+        pathname: "/**",
       },
-}
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
